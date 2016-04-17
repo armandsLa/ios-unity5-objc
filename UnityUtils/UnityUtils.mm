7953084e3f8a1d368643411ab823178ca5c040db
Armands Lazdiņš <armands.la@gmail.com>
2016-04-17 20:26:53 +0300

    UnityUtils updated to Objective-C

--- UnityUtils/UnityUtils.mm
+++ UnityUtils/UnityUtils.mm
@@ -12,7 +12,6 @@
 #include "RegisterFeatures.h"
 #include <csignal>
 
-
 // Hack to work around iOS SDK 4.3 linker problem
 // we need at least one __TEXT, __const section entry in main application .o files
 // to get this section emitted at right time and so avoid LC_ENCRYPTION_INFO size miscalculation
@@ -20,18 +19,23 @@ static const int constsection = 0;
 
 void UnityInitTrampoline();
 
-
-extern "C" void custom_unity_init(int argc, char* argv[]){
-    UnityInitTrampoline();
-    UnityParseCommandLine(argc, argv);
-
-    RegisterMonoModules();
-    NSLog(@"-> registered mono modules %p\n", &constsection);
-    RegisterFeatures();
-
-    // iOS terminates open sockets when an application enters background mode.
-    // The next write to any of such socket causes SIGPIPE signal being raised,
-    // even if the request has been done from scripting side. This disables the
-    // signal and allows Mono to throw a proper C# exception.
-    std::signal(SIGPIPE, SIG_IGN);
+int custom_unity_init(int argc, char* argv[])
+{
+    @autoreleasepool
+    {
+        UnityInitTrampoline();
+        UnityParseCommandLine(argc, argv);
+        
+        RegisterMonoModules();
+        NSLog(@"-> registered mono modules %p\n", &constsection);
+        RegisterFeatures();
+        
+        // iOS terminates open sockets when an application enters background mode.
+        // The next write to any of such socket causes SIGPIPE signal being raised,
+        // even if the request has been done from scripting side. This disables the
+        // signal and allows Mono to throw a proper C# exception.
+        std::signal(SIGPIPE, SIG_IGN);
+    }
+    
+    return 0;
 }
